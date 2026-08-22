f23715038fee0bb1ce622541c46cf8c093995807
Artanniel Fortes <fortesads@gmail.com>
2026-08-22 16:05:51 -0300

    fix: disable automatic schema initialization in test and MySQL configurations and remove redundant Testcontainers annotation

--- src/test/java/com/artantech/paymentservice/integration/PaymentApiIntegrationTest.java
+++ src/test/java/com/artantech/paymentservice/integration/PaymentApiIntegrationTest.java
@@ -20,7 +20,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -39,7 +38,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Testcontainers
 @ActiveProfiles("mysql")
 class PaymentApiIntegrationTest {
 
